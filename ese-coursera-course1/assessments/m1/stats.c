35dc3f4847be2c1b21b8c5742b2b29ebfdd85cde
devC-mcu <43113509+devC-mcu@users.noreply.github.com>
2018-10-08 00:12:52 +0530

    Update stats.c

--- ese-coursera-course1/assessments/m1/stats.c
+++ ese-coursera-course1/assessments/m1/stats.c
@@ -37,7 +37,115 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+  printf("Statistics:\n");
+  print_statistics(test, SIZE);
 
+  printf("Array:\n");
+  print_array(test, SIZE);
+
+  printf("Sorted Array:\n");
+  sort_array(test, SIZE);
+  print_array(test, SIZE);
+
+}
+
+/* Add other Implementation File Code Here */
+
+void print_statistics(unsigned char* array1, unsigned int length){
+
+  unsigned char minimum = 0;
+  unsigned char maximum = 0;
+  unsigned char mean = 0;
+  unsigned char median = 0;
+
+  minimum = find_minimum(array1, length);
+  maximum = find_maximum(array1, length);
+  mean = find_mean(array1, length);
+  median = find_median(array1, length);
+
+  printf("\n"); // Empty line before the statistics
+  printf("Minimum Value= %d\n", minimum);
+  printf("Maximum Value= %d\n", maximum);
+  printf("Mean Value= %d\n", mean);
+  printf("Median Value=  %d\n", median);
+}
+
+void print_array(unsigned char* array1, unsigned int length){
+  printf("\n"); // Empty line before the array
+
+  for(unsigned int i = 0; i < length; i++){
+    printf("%d\t", array1[i]);
+
+  }
+}
+
+unsigned char find_median(unsigned char* array1, unsigned int length){
+  unsigned char median = 0;
+  unsigned char array2[length];
+
+  for(unsigned int i = 0; i<length; i++){
+    array2[i] = array1[i];
+  }
+
+  sort_array(array2, length);
+  if(length%2 == 0)
+     {
+        median = array2[length/2];
+     }
+  else
+    median = array2[(length+1)/2];
+
+  return median;
+}
+
+unsigned char find_mean(unsigned char* array1, unsigned int length){
+  unsigned char mean = 0;
+  unsigned long int sum = 0;
+
+  for (unsigned int i = 0; i < length; i ++) {
+    sum += array1[i];
+  }
+
+  mean = sum/length;
+
+  return mean;
+}
+
+unsigned char find_maximum(unsigned char* array1, unsigned int length){
+  unsigned char maximum = array1[0];
+
+  for (unsigned int i = 0; i < length; i ++) {
+    if(array1[i] > maximum) maximum = array1[i];
+  }
+
+  return maximum;
+}
+
+unsigned char find_minimum(unsigned char* array1, unsigned int length){
+  unsigned char minimum = array1[0];
+
+  for (unsigned int i = 0; i < length; i ++) {
+    if(array1[i] < minimum) minimum = array1[i];
+  }
+
+  return minimum;
+}
+
+void sort_array(unsigned char* array1, unsigned int length){
+  unsigned char temp;
+
+  for(unsigned int i = 0; i < length; i++)
+  {
+  	  for(unsigned int j = 0; j < length-1; j++)
+  	  {
+  			   if(array1[j] < array1[i])
+  			   {
+  				   temp = array1[i];
+  				   array1[i] = array1[j];
+  				   array1[j] = temp;
+  				}
+  	   }
+  }
 }
 
 /* Add other Implementation File Code Here */
